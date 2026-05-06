e6143038c18efff93d567238530c279e0f18f4a3
Yves <yvesfogel@gmail.com>
2026-05-06 14:10:26 -0300

    Fix build: remove unused Link import from Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { siteConfig } from "../config/site";
 import programData from "../data/program.json";
 import partnersData from "../data/partners.json";
